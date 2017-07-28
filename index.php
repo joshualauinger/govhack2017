f33f04dd2e289509b04b2a4c0252ea146a1d3530
joshualauinger <j.lauinger@hotmail.com>
2017-07-28 22:49:30 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Govhack Project 2017</title>
+    </head>
+    <body>
+        <p>Hello world, but more specifically Daniel & Tom</p>
+    </body>
+</html>
